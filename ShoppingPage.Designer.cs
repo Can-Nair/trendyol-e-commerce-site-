51a2e227ebf9d539564c3601b9a02fe1c5f09fdd
Can-Nair <100345154+Can-Nair@users.noreply.github.com>
2023-01-03 15:36:57 +0300

    Shopping ekranı eklendi

---
+++ ShoppingPage.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace trendyol
+{
+    partial class ShoppingPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxcash = new System.Windows.Forms.TextBox();
+            this.textBoxDate = new System.Windows.Forms.TextBox();
+            this.textBoxCustomerID = new System.Windows.Forms.TextBox();
+            this.textBoxQuantity = new System.Windows.Forms.TextBox();
+            this.textBoxProductPrice = new System.Windows.Forms.TextBox();
+            this.textBoxProductName = new System.Windows.Forms.TextBox();
+            this.textBoxProductID = new System.Windows.Forms.TextBox();
+            this.gvProductList = new System.Windows.Forms.DataGridView();
+            this.cbCategoryType = new System.Windows.Forms.ComboBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.gvShoppingCart = new System.Windows.Forms.DataGridView();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gvProductList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvShoppingCart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cash";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CustomerID";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(919, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Product ID";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(919, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Product Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(936, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Price";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(919, 178);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(55, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Quantity";
+            // 
+            // textBoxcash
+            // 
+            this.textBoxcash.Location = new System.Drawing.Point(109, 30);
+            this.textBoxcash.Name = "textBoxcash";
+            this.textBoxcash.Size = new System.Drawing.Size(100, 22);
+            this.textBoxcash.TabIndex = 7;
+            // 
+            // textBoxDate
+            // 
+            this.textBoxDate.Location = new System.Drawing.Point(109, 86);
+            this.textBoxDate.Name = "textBoxDate";
+            this.textBoxDate.Size = new System.Drawing.Size(100, 22);
+            this.textBoxDate.TabIndex = 8;
+            // 
+            // textBoxCustomerID
+            // 
+            this.textBoxCustomerID.Location = new System.Drawing.Point(109, 138);
+            this.textBoxCustomerID.Name = "textBoxCustomerID";
+            this.textBoxCustomerID.Size = new System.Drawing.Size(100, 22);
+            this.textBoxCustomerID.TabIndex = 9;
+            // 
+            // textBoxQuantity
+            // 
+            this.textBoxQuantity.Location = new System.Drawing.Point(1026, 178);
+            this.textBoxQuantity.Name = "textBoxQuantity";
+            this.textBoxQuantity.Size = new System.Drawing.Size(92, 22);
+            this.textBoxQuantity.TabIndex = 10;
+            // 
+            // textBoxProductPrice
+            // 
+            this.textBoxProductPrice.Location = new System.Drawing.Point(1026, 135);
+            this.textBoxProductPrice.Name = "textBoxProductPrice";
+            this.textBoxProductPrice.Size = new System.Drawing.Size(92, 22);
+            this.textBoxProductPrice.TabIndex = 11;
+            // 
+            // textBoxProductName
+            // 
+            this.textBoxProductName.Location = new System.Drawing.Point(1026, 86);
+            this.textBoxProductName.Name = "textBoxProductName";
+            this.textBoxProductName.Size = new System.Drawing.Size(92, 22);
+            this.textBoxProductName.TabIndex = 12;
+            // 
+            // textBoxProductID
+            // 
+            this.textBoxProductID.Location = new System.Drawing.Point(1026, 41);
+            this.textBoxProductID.Name = "textBoxProductID";
+            this.textBoxProductID.Size = new System.Drawing.Size(92, 22);
+            this.textBoxProductID.TabIndex = 13;
+            // 
+            // gvProductList
+            // 
+            this.gvProductList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvProductList.Location = new System.Drawing.Point(238, 120);
+            this.gvProductList.Name = "gvProductList";
+            this.gvProductList.RowHeadersWidth = 51;
+            this.gvProductList.RowTemplate.Height = 24;
+            this.gvProductList.Size = new System.Drawing.Size(654, 231);
+            this.gvProductList.TabIndex = 18;
+            // 
+            // cbCategoryType
+            // 
+            this.cbCategoryType.FormattingEnabled = true;
+            this.cbCategoryType.Location = new System.Drawing.Point(380, 58);
+            this.cbCategoryType.Name = "cbCategoryType";
+            this.cbCategoryType.Size = new System.Drawing.Size(121, 24);
+            this.cbCategoryType.TabIndex = 19;
+            this.cbCategoryType.SelectedIndexChanged += new System.EventHandler(this.cbCategoryType_SelectedIndexChanged);
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(639, 58);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(75, 23);
+            this.btnList.TabIndex = 20;
+            this.btnList.Text = "List";
+            this.btnList.UseVisualStyleBackColor = true;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // gvShoppingCart
+            // 
+            this.gvShoppingCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvShoppingCart.Location = new System.Drawing.Point(277, 501);
+            this.gvShoppingCart.Name = "gvShoppingCart";
+            this.gvShoppingCart.RowHeadersWidth = 51;
+            this.gvShoppingCart.RowTemplate.Height = 24;
+            this.gvShoppingCart.Size = new System.Drawing.Size(576, 143);
+            this.gvShoppingCart.TabIndex = 21;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.Location = new System.Drawing.Point(369, 391);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Size = new System.Drawing.Size(98, 37);
+            this.btnOrder.TabIndex = 22;
+            this.btnOrder.Text = "Order";
+            this.btnOrder.UseVisualStyleBackColor = true;
+            this.btnOrder.Click += new System.EventHandler(this.btnOrder_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(639, 391);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(107, 37);
+            this.btnDelete.TabIndex = 23;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(1026, 226);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(75, 23);
+            this.btnRefresh.TabIndex = 24;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1156, 684);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnOrder);
+            this.Controls.Add(this.gvShoppingCart);
+            this.Controls.Add(this.btnList);
+            this.Controls.Add(this.cbCategoryType);
+            this.Controls.Add(this.gvProductList);
+            this.Controls.Add(this.textBoxProductID);
+            this.Controls.Add(this.textBoxProductName);
+            this.Controls.Add(this.textBoxProductPrice);
+            this.Controls.Add(this.textBoxQuantity);
+            this.Controls.Add(this.textBoxCustomerID);
+            this.Controls.Add(this.textBoxDate);
+            this.Controls.Add(this.textBoxcash);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvProductList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gvShoppingCart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxcash;
+        private System.Windows.Forms.TextBox textBoxDate;
+        private System.Windows.Forms.TextBox textBoxCustomerID;
+        private System.Windows.Forms.TextBox textBoxQuantity;
+        private System.Windows.Forms.TextBox textBoxProductPrice;
+        private System.Windows.Forms.TextBox textBoxProductName;
+        private System.Windows.Forms.TextBox textBoxProductID;
+        private System.Windows.Forms.DataGridView gvProductList;
+        private System.Windows.Forms.ComboBox cbCategoryType;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.DataGridView gvShoppingCart;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnRefresh;
+    }
+}
---
+++ ShoppingPage.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trendyol
+{
+    public partial class ShoppingPage : Form
+    {
+        private readonly trendyolEntities trendyolEntities = new trendyolEntities();
+        public ShoppingPage()
+        {
+            InitializeComponent();
+            textBoxDate.Text = DateTime.Now.ToLongDateString();
+            textBoxCustomerID.Text = Convert.ToString(4);
+            var cust_id = Convert.ToInt32(textBoxCustomerID.Text);
+            textBoxcash.Text = Convert.ToString((from q in trendyolEntities.customers where q.CustomerID == cust_id select q.Cash).Single());
+
+            //var customerCash = from q in trendyolDemoDBEntities.customers where q.CustomerID == Convert.ToInt32(textBoxCustomerID.Text) select q.cash;
+        }
+
+        void loadData()
+        {
+
+            var st = from s in trendyolEntities.products select s;
+            gvShoppingCart.DataSource = st;
+
+        }
+
+        int grandTotal = 0, n = 0;
+        //int total_amount = 0;
+        Decimal Account = 0;
+
+        private void cbCategoryType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var categories = trendyolEntities.categories.ToList();
+            cbCategoryType.DisplayMember = "CategoryName";
+            cbCategoryType.ValueMember = "CategoryID";
+            cbCategoryType.DataSource = categories;
+            trendyolEntities.SaveChanges();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            var category_name = cbCategoryType.Text;
+            //if (category_name.Text == "")
+            var is_appropriate = Convert.ToInt32((from q in trendyolEntities.categories where q.CategoryName == category_name join p in trendyolEntities.products on q.CategoryID equals p.CategoryID select p.isInappropriate).Single());
+            if (is_appropriate == 1)
+            {
+                MessageBox.Show("+18 Staff", "Window Title", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+            Console.WriteLine(category_name);
+            var catID = trendyolEntities.categories.Where(x => x.CategoryName == category_name).Select(x => x.CategoryID).ToList();
+            int i = catID[0];
+            Console.WriteLine(i);
+            var prd = trendyolEntities.products.Where(q => q.CategoryID == i).Select(q => new { q.ProductID, q.ProductName, q.SupplierID, q.CategoryID, q.stock, q.Price }).ToList();
+            gvProductList.DataSource = prd.ToList();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            int rowIndex = gvShoppingCart.CurrentCell.RowIndex;
+            Console.WriteLine("row index : " + rowIndex);
+            int p_id = Convert.ToInt32(gvShoppingCart.CurrentRow.Cells["ProductID"].Value);
+            DataGridViewRow row = gvShoppingCart.Rows[rowIndex];
+            string p_name = Convert.ToString(row.Cells["ProductName"].Value);
+            var p_price = Convert.ToDouble((from p in trendyolEntities.products where p.ProductID == p_id select p.Price).Single());
+            Console.WriteLine("p_price : " + p_price);
+            int stok_amount = Convert.ToInt32(row.Cells["ProductQuantity"].Value);
+            gvShoppingCart.Rows.RemoveAt(rowIndex);
+
+            var query = from ord in trendyolEntities.products where ord.ProductName == p_name select ord;
+            foreach (product i in query)
+            {
+                i.stock += stok_amount;
+            }
+
+            var total_cash = Convert.ToDouble(textBoxcash.Text);
+            total_cash += (p_price * stok_amount);
+            textBoxcash.Text = Convert.ToString(total_cash);
+
+            var cust_id = Convert.ToInt32(textBoxCustomerID.Text);
+            trendyolEntities.customers.Where(p => p.CustomerID == cust_id).ToList().ForEach(x => x.Cash = total_cash);
+            Console.WriteLine("p_id : " + p_id);
+
+            var deleted_rows = (from od in trendyolEntities.order_details
+                                join o in trendyolEntities.orders on od.OrderID equals o.OrderID
+                                where od.ProductID == p_id
+                                //select new { order_id = o.OrderID, order_details_order_id = od.OrderID }).FirstOrDefault();
+                                select new { order_id = o, od_order_id = od });
+            foreach (var item in deleted_rows)
+            {
+                trendyolEntities.order_details.Remove(item.od_order_id);
+                trendyolEntities.orders.Remove(item.order_id);
+            }
+            trendyolEntities.SaveChanges();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            textBoxProductName.Text = gvProductList.SelectedRows[0].Cells["ProductName"].Value.ToString();
+            textBoxProductPrice.Text = gvProductList.SelectedRows[0].Cells["Price"].Value.ToString();
+            textBoxProductID.Text = gvProductList.SelectedRows[0].Cells["ProductID"].Value.ToString();
+            textBoxQuantity.Clear();
+        }
+
+        private void btnOrder_Click(object sender, EventArgs e)
+        {
+            DateTime orderDate = DateTime.Parse(textBoxDate.Text);
+            var cust_id = Convert.ToInt32(textBoxCustomerID.Text);
+            var customerCash = Convert.ToDouble((from q in trendyolEntities.customers where q.CustomerID == cust_id select q.Cash).Single());
+            order add_order = new order();
+            add_order.OrderID = n;
+            add_order.CustomerID = Convert.ToInt32(textBoxCustomerID.Text);
+            add_order.OrderDate = orderDate;
+            trendyolEntities.orders.Add(add_order);
+            order_details order_items = new order_details();
+            order_items.Quantity = Convert.ToInt32(textBoxQuantity.Text);
+            order_items.OrderID = add_order.OrderID;
+            order_items.OrderDetailID = n;
+            order_items.ProductID = trendyolEntities.products.First(a => a.ProductName == textBoxProductName.Text).ProductID;
+            var totalAmount = (from p in trendyolEntities.customers
+                               join c in trendyolEntities.orders on p.CustomerID equals c.CustomerID
+                               join a in trendyolEntities.order_details on c.OrderID equals a.OrderID
+                               join d in trendyolEntities.products on a.ProductID equals d.ProductID
+                               where d.ProductID == order_items.ProductID
+                               select new
+                               {
+                                   //Quantity = a.Quantity,
+                                   //TotalAmount = a.Quantity * Convert.ToInt32(textBoxpprice.Text)
+                                   TotalAmount = (order_items.Quantity * d.Price)
+                               });
+            Console.WriteLine("Quantity :" + order_items.Quantity);
+            Console.WriteLine("Product ID :" + order_items.ProductID);
+
+            trendyolEntities.order_details.Add(order_items);
+
+
+            foreach (var item in totalAmount)
+            {
+                Console.WriteLine("total : " + item.TotalAmount);
+            }
+
+            var total_amount = (from l in totalAmount orderby l.TotalAmount select l.TotalAmount).First();
+
+            Console.WriteLine("Last total : " + total_amount);
+
+            var p_name = textBoxProductName.Text;
+            var quantity = textBoxQuantity.Text;
+
+
+            var query = from ord in trendyolEntities.products where ord.ProductName == p_name select ord;
+            var product_stock = 0;
+
+            if (textBoxcash.Text == "" || textBoxProductName.Text == "" || textBoxQuantity.Text == "")
+            {
+                MessageBox.Show("Missing Information", "Information Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+
+            foreach (product i in query)
+            {
+                product_stock = i.stock;
+            }
+
+            if (Convert.ToInt32(textBoxQuantity.Text) > product_stock || (product_stock - Convert.ToInt32(textBoxQuantity.Text)) < 0)
+            {
+                MessageBox.Show("No Stock", "Information Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+            }
+
+            var category_name = cbCategoryType.Text;
+            var is_appropriate = Convert.ToInt32((from q in trendyolEntities.categories where q.CategoryName == category_name join p in trendyolEntities.products on q.CategoryID equals p.CategoryID select p.isInappropriate).Single());
+            var is_hidden = Convert.ToInt32((from q in trendyolEntities.categories where q.CategoryName == category_name join p in trendyolEntities.products on q.CategoryID equals p.CategoryID select p.isHidden).Single());
+
+            if (is_appropriate == 1 && is_hidden == 1)
+            {
+                MessageBox.Show("It is hidden to buy. You cannot buy this product", "Window Title", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+
+            else
+            {
+                foreach (product i in query)
+                {
+                    product_stock = i.stock;
+                    //Console.WriteLine("Stock : ", product_stock);
+                }
+
+                if (customerCash <= 0 || customerCash - total_amount <= 0)
+                {
+
+                    MessageBox.Show("No Money or No Stock", "Information Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+
+                DataGridViewRow addRow = new DataGridViewRow();
+                addRow.CreateCells(gvShoppingCart);
+                addRow.Cells[0].Value = textBoxProductID.Text;
+                addRow.Cells[1].Value = textBoxProductName.Text;
+                addRow.Cells[2].Value = textBoxProductPrice.Text;
+                addRow.Cells[3].Value = textBoxQuantity.Text; addRow.Cells[4].Value = Convert.ToDouble(total_amount);
+                gvShoppingCart.Rows.Add(addRow);
+
+                customerCash = customerCash - Convert.ToDouble(total_amount);
+                trendyolEntities.customers.Where(p => p.CustomerID == cust_id).ToList().ForEach(x => x.Cash = customerCash);
+
+                int stok_amount = Convert.ToInt32(quantity);
+                Console.WriteLine("stok_amount : " + stok_amount);
+
+
+                foreach (product i in query)
+                {
+
+                    if (i.stock <= 0 || i.stock - Convert.ToInt32(textBoxQuantity.Text) <= 0)
+                    {
+
+
+                        MessageBox.Show("No Stock", "Information Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+
+                    }
+
+                    i.stock -= stok_amount;
+
+
+
+                    //trendyolDBEntities.SaveChanges();
+
+                }
+
+                textBoxcash.Text = Convert.ToString(customerCash);
+
+                trendyolEntities.SaveChanges();
+            }
+
+            n++;
+        }
+
+    }
+}
